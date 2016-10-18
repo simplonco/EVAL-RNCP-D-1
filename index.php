0f8f1a7bf45ab247241d0f5c3ac5fa9457b04a2a
Elmardi2 <mardi.yahia@gmail.com>
2016-10-18 15:12:43 +0200

    upload fichiers

---
+++ index.php
@@ -0,0 +1,89 @@
+
+<?php
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Konexion</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/jquery.bxslider.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    <!--Start first page -->
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="col-sx-12">
+          test1
+        </div>
+      </div>
+    </div>
+      <!--End first page -->
+    <!--Start list  page -->
+    <div class="container">
+      <ul id="drop-nav">
+  <li><a href="#">Inroduction
+    <ul>
+      <li><a href="#">About Konexio</a></li>
+      <li><a href="#">Our Mission</a></li>
+    </ul>
+  </a></li>
+  <li><a href="#">Team & partners</a>
+    <ul>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+    </ul>
+  </li>
+  <li><a href="#">Main Activities</a>
+    <ul>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+    </ul>
+  </li>
+  <li><a href="#">Contact</a>
+    <ul>
+      <li><a href="#">Details</a></li>
+      <li><a href="#">Ask me a Question</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Testimonial </a>
+    <ul>
+      <li><a href="#">Testimonial video</a></li>
+      <li><a href="#">Testimonial text </a></li>
+    </ul>
+  </li>
+</ul>
+    </div>
+    <!--End list page -->
+      <!--Start Inroduction  page -->
+    <div class="container">
+      <h1>About Konexio</h1>
+      <p> – a few paragraphs on objectives of the organization</p>
+      <h1>Our Mission</h1>
+      <p>
+       With the recent migrant crisis impacting Europe and the world on an unprecedented scale, our mission is to contribute to the process of integration of refugees into these communities through innovation and technology.
+       Refugees face a number of tough challenges once their asylum status has been obtained, but one of the most critical and most burdensome problems for European nations medium to longer term will be that of economic integration through job opportunity.
+       Our organization aims to tackle this by providing refugees with opportunities to learn and connect with through code.
+      </p>
+    </div>
+      <!--End Introduction page -->
+      <!--Start Main Activities page -->
+      <div class="container">
+        
+      </div>
+      <!--End Main Activities page -->
+
+   <script src="js/jquery-3.1.0.min.js"></script>
+   <script src="js/jquery.mixitup.js"></script>
+   <script src="js/jquery.bxslider.min.js"></script>
+   <script src="js/test.js"></script>
+  </body>
+</html>
