79adb0eb3bc36e702d67cbed284d635348f7941b
Elmardi2 <mardi.yahia@gmail.com>
2016-11-02 21:48:12 +0100

    set img

--- js/func.js
+++ js/func.js
@@ -9,12 +9,12 @@ $(function () {
   (function AutoSlider() {
     $('.slid .act').each(function () {
       if (!$(this).is(':last-child')){
-      $(this).fadeOut(2000,function () {
+      $(this).fadeOut(3000,function () {
          $(this).removeClass('act').next().addClass('act').fadeIn();
          AutoSlider();
       });
     }else {
-      $(this).fadeOut(1000,function () {
+      $(this).fadeOut(2000,function () {
          $(this).removeClass('act');
       $('.slid div').eq(0).addClass('act').fadeIn();
        AutoSlider();
@@ -27,12 +27,12 @@ $(function () {
   (function AutoSlider() {
     $('.slider .active').each(function () {
       if (!$(this).is(':last-child')){
-      $(this).delay(1000).fadeOut(1000,function () {
+      $(this).delay(2000).fadeOut(2000,function () {
          $(this).removeClass('active').next().addClass('active').fadeIn();
          AutoSlider();
       });
     }else {
-      $(this).delay(3000).fadeOut(2000,function () {
+      $(this).delay(1000).fadeOut(1000,function () {
          $(this).removeClass('active');
       $('.slider div').eq(0).addClass('active').fadeIn();
        AutoSlider();
